7a4806fed51b06429a156cb4b7cafcba2296455f
KhuatQuangMinh2051060611 <73015543+minhpsapc@users.noreply.github.com>
2023-08-18 16:54:47 +0700

    TESTCUOI

--- testgiuaky/resources/views/master.blade.php
+++ testgiuaky/resources/views/master.blade.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container mt-5">
 
-        <h1 class="text-primary mt-3 mb-4 text-center"><b>Channel Faketube</b></h1>
+        <h1 class="text-primary mt-3 mb-4 text-center"><b>CHANNEL FAKETUBE</b></h1>
 
         @yield('content')
 
